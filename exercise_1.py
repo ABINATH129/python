97117a546fab7b510d29ff0ca7e69035510f77d1
ABINATH129 <146427459+ABINATH129@users.noreply.github.com>
2025-05-14 09:07:06 +0530

    Add files via upload

---
+++ exercise_1.py
@@ -0,0 +1,11 @@
+# find the area of rectangle
+
+# formula is A=w*l
+
+width=int(input("enter the width of a rectangle:"))
+length=int(input("enter the length of a rectangle:"))
+area=width*length
+print(f"area of rectangle is {area}cm")
+
+area=float(area)
+print(f"area of rectangle is : {area}cm")
---
+++ exercise_2.py
@@ -0,0 +1,8 @@
+# shoping a cart
+item = input("enter the item:")
+quantity = int(input("enter the quantity you want:"))
+price = float(input("price of a iten :"))
+
+amount = quantity * price
+
+print(f"amount is ₹{amount} ")
